247fe571f517f23482a7a8994f5546781febdfff
Ameen Abdulsamad <amenchawshly98@gmail.com>
2022-12-20 01:00:27 +0300

    Doc: add new files \ create Pages and home folder\

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
        <h1>crazy minds</h1>
+       <Home/>
       </header>
     </div>
   );
