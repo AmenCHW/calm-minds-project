67b50b8ef4edd96942df20f43d239dda0a9e7df0
Danah <osta.danah@gmail.com>
2022-12-21 21:35:54 +0200

    style: enhance style

--- src/pages/Home/BookingButton.js
+++ src/pages/Home/BookingButton.js
@@ -14,7 +14,7 @@ import React from 'react';
              <div className="Booking">
               
                <button  
-               className="mx-72 my-20 bg-blue-500 hover:bg-blue-400 text-white 
+               className="mt-20 bg-blue-500 hover:bg-blue-400 text-white 
                font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded" 
                 type="button" >
                   BOOK AN APPOINTMENT
--- src/pages/Home/BookingSection.js
+++ src/pages/Home/BookingSection.js
@@ -11,21 +11,21 @@ import img5 from "../../img/img4.PNG"
 
 function BookingSection() {
   return (
-    <div className="Booking bg-amber-200">
+    <div className="Booking bg-amber-200 mx-auto px-24 py-10">
       <header className="BookingHeader">
-       <h1 className="ml-80  text-6xl font-normal leading-normal mt-0 mb-2 text-slate-800   ">Professional, licensed, and vetted <br/>therapists that you can trust</h1>
+       <h1 className=" text-3xl md:text-6xl font-normal leading-normal mt-0 mb-2 text-slate-800">Professional, licensed, and vetted <br/>therapists that you can trust</h1>
        
       </header>
       <body>
-        <div className='h-56 ml-64  flex px-0 items-stretch  '>
-          <img className='rounded-full  scale-50 border-solid border-4 border-blue-600' src={img1}   alt="therapiest"/>
-          <img  className='rounded-full scale-50  border-solid border-4 border-blue-600' src={img2}   alt="therapiest"/>
-          <img className='rounded-full scale-50 border-solid border-4 border-blue-600' src={img3}   alt="therapiest"/>
-          <img className='rounded-full scale-50 border-solid border-4 border-blue-600' src={img4}   alt="therapiest"/>
-          <img className='rounded-full scale-50 border-solid border-4 border-blue-600' src={img5}   alt="therapiest"/>
+        <div className='py-10 flex-wrap md:flex '>
+          <img className='mx-2 rounded-full w-[7%] h-full border-solid border-4 border-blue-600' src={img1}   alt="therapiest"/>
+          <img  className='mx-2 rounded-full w-[7%] h-full border-solid border-4 border-blue-600' src={img2}   alt="therapiest"/>
+          <img className='mx-2 rounded-full w-[7%] border-solid border-4 border-blue-600' src={img3}   alt="therapiest"/>
+          <img className='mx-2 rounded-full w-[7%] border-solid border-4 border-blue-600' src={img4}   alt="therapiest"/>
+          <img className='mx-2 rounded-full w-[7%] border-solid border-4 border-blue-600' src={img5}   alt="therapiest"/>
         </div>
          
-        <p   className="text-justify ml-72 mt-2 ">
+        <p   className="text-justify mt-2 ">
           Tap into the worlds largest network of licensed, accredited, and experienced therapists who can help you with<br/>
          a range of issues including depression, anxiety, relationships, trauma, grief, and more. with our therapists,<br/>
          you get the same professionalism and quality you would expect from an in-office therapist, but with the ability to<br/>
