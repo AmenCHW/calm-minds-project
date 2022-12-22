d9d65eaee086bfdf49db6ad922e7a66da8459bc5
hudahamid <huda.00091581@gmail.com>
2022-12-22 23:38:10 +0300

    fix the style

--- src/pages/Booking/BookingButton.js
+++ src/pages/Booking/BookingButton.js
@@ -9,8 +9,8 @@ import { Link } from 'react-router-dom';
              <div className="Booking">
               <Link to="/bookingPage" className="focus:text-[#FEE89E]">
                 <button  
-                  className="mt-20 bg-blue-500 hover:bg-blue-400 text-white 
-                  font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded" 
+                  className="mt-20 bg-[#2DD3E3] hover:bg-blue-400 text-white 
+                  font-bold py-2 px-4 border-b-4 border-[#2DD3E3] hover:border-blue-500 rounded" 
                   type="button" >
                    BOOK AN APPOINTMENT    
                 </button> 
--- src/pages/Booking/BookingSectionHome.js
+++ src/pages/Booking/BookingSectionHome.js
@@ -2,34 +2,34 @@ import React from 'react';
 
  import BookingButton from './BookingButton';
 
-import img1 from "../../img/chk.PNG"
-import img2 from "../../img/img.PNG"
-import img3 from "../../img/img2.PNG"
-import img4 from "../../img/img3.PNG"
-import img5 from "../../img/img4.PNG"
+// import img1 from "../../img/chk.PNG"
+// import img2 from "../../img/img.PNG"
+// import img3 from "../../img/img2.PNG"
+// import img4 from "../../img/img3.PNG"
+import img from "../../img/img4.PNG"
 
 
 
 function BookingSection() {
   return (
-    <div className="Booking bg-amber-200 mx-auto px-24 py-10">
+    <div className="Booking bg-amber-200 mx-auto lg:px-44 px-10 py-14">
       <header className="BookingHeader">
-       <h1 className=" text-3xl md:text-6xl font-normal leading-normal mt-0 mb-2 text-slate-800">Professional, licensed, and vetted <br/>therapists that you can trust</h1>
+       <h1 className="text-4xl md:text-5xl lg:text-6xl font-normal leading-normal mt-0 mb-2 text-slate-800 lg:pr-14">  PROFESSIONAL, LICENSED, AND VETTED THERAPISTS THAT YOU CAN TRUST</h1>
        
       </header>
       <body>
-        <div className='py-10 flex-wrap md:flex '>
-          <img className='mx-2 rounded-full w-[7%] h-full border-solid border-4 border-blue-600' src={img1}   alt="therapiest"/>
-          <img  className='mx-2 rounded-full w-[7%] h-full border-solid border-4 border-blue-600' src={img2}   alt="therapiest"/>
-          <img className='mx-2 rounded-full w-[7%] border-solid border-4 border-blue-600' src={img3}   alt="therapiest"/>
-          <img className='mx-2 rounded-full w-[7%] border-solid border-4 border-blue-600' src={img4}   alt="therapiest"/>
-          <img className='mx-2 rounded-full w-[7%] border-solid border-4 border-blue-600' src={img5}   alt="therapiest"/>
+        <div className='py-10 flex flex-wrap mx-auto md:mx-0'>
+          <img className='mx-2 rounded-full my-4 md:my-0 w-36 h-full border-solid border-2 border-[#2DD3E3]' src={img}   alt="therapiest"/>
+          <img  className='mx-2 rounded-full my-4 md:my-0 w-36 h-full border-solid border-2 border-[#2DD3E3]' src={img}   alt="therapiest"/>
+          <img className='mx-2 rounded-full my-4 md:my-0 w-36  h-full border-solid border-2 border-[#2DD3E3]' src={img}   alt="therapiest"/>
+          <img className='mx-2 rounded-full my-4 md:my-0 w-36  h-full border-solid border-2 border-[#2DD3E3]' src={img}   alt="therapiest"/>
+          <img className='mx-2 rounded-full my-4 md:my-0 w-36  h-full border-solid border-2 border-[#2DD3E3]' src={img}   alt="therapiest"/>
         </div>
          
-        <p   className="text-justify mt-2 ">
-          Tap into the worlds largest network of licensed, accredited, and experienced therapists who can help you with<br/>
-         a range of issues including depression, anxiety, relationships, trauma, grief, and more. with our therapists,<br/>
-         you get the same professionalism and quality you would expect from an in-office therapist, but with the ability to<br/>
+        <p className="lg:text-justify mt-2 text-2xl">
+          Tap into the worlds largest network of licensed, accredited, and experienced therapists who can help you with
+         a range of issues including depression, anxiety, relationships, trauma, grief, and more. With our therapists,
+         you get the same professionalism and quality you would expect from an in-office therapist, but with the ability to
           communicate whenever and however you want.
         </p>
 
