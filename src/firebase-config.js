a91b14ca0ad4ca78174e612b6f548ce2fba05698
Ameen Abdulsamad <amenchawshly98@gmail.com>
2022-12-24 20:22:36 +0300

    Refactor: add firebase env file
    add firebase api to an env file
    Fixes #15

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from '@firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDY-EfG2iUbCSILU2iQCAVXYjLmLCI4Pao",
+    apiKey: `${process.env.REACT_APP_UNSPLASH_KEY}`,
     authDomain: "calm-minds-project.firebaseapp.com",
     projectId: "calm-minds-project",
     storageBucket: "calm-minds-project.appspot.com",
