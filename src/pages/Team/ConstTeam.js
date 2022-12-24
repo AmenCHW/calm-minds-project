5b5da71dbcd34588ebfbee33d23845281d8d28a4
hudahamid <huda.00091581@gmail.com>
2022-12-24 23:51:58 +0300

    .

---
+++ src/pages/Team/ConstTeam.js
@@ -0,0 +1,53 @@
+import img1 from './img/Rectangle1.png';
+import img2 from './img/Rectangle2.png';
+import img3 from './img/Rectangle3.png';
+import img4 from './img/Rectangle4.png';
+import img5 from './img/Rectangle 5.png';
+import img6 from './img/Rectangle.png';
+
+const constTeam = [
+  {
+    id: 1,
+    src: img1,
+    alt: 'img1',
+    Name: 'Allan Saleh',
+    title: 'Lead Engineer& Web Developer',
+  },
+  {
+    id: 2,
+    src: img2,
+    alt: 'img2',
+    Name: 'Payam Abubakr',
+    title: 'Junior Designer& FE Developer',
+  },
+  {
+    id: 3,
+    src: img3,
+    alt: 'img3',
+    Name: 'Ahmed Mohammed',
+    title: 'Junior Designer& FE Developer',
+  },
+  {
+    id: 4,
+    src: img4,
+    alt: 'img4',
+    Name: 'Abdulmajeed Jaafer',
+    title: 'Junior Designer& FE Developer',
+  },
+  {
+    id: 5,
+    src: img5,
+    alt: 'img5',
+    Name: 'Snoor Madih',
+    title: 'Junior Designer& FE Developer',
+  },
+  {
+    id: 6,
+    src: img6,
+    alt: 'img6',
+    Name: 'Avraz Zebary',
+    title: 'Junior Designer& FE Developer',
+  },
+];
+
+export default constTeam;
--- src/pages/Team/Team.js
+++ src/pages/Team/Team.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import constTeam from './ConstTeam';
 
 function Team() {
-  return <h1 className="mx-auto text-center text-7xl">Team</h1>;
+  return (
+    <div >
+      <h1 className="mx-auto text-center text-5xl">
+        WE ARE HEALING, NICE TO MEET YOU!
+      </h1>
+      <h4 className="mx-auto text-center text-3xl">Meet the Team!</h4>
+      <div className="flex flex-wrap mt-44">
+       
+      {constTeam.map((element) => {
+        return (
+          <div
+            key={element.id}
+            className="flex flex-col border-1 
+       p-3 text-center justify-center items-center mx-1"
+          >
+            <img className="w-16 h-16" src={element.src} alt={element.alt} />
+            <h4> {element.Name}</h4>
+            <h5>{element.title}</h5>
+          </div>
+        );
+      })}</div>
+    </div>
+  );
 }
 
 export default Team;
