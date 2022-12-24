8d23b9b518d0b1c905dac55ef63d443f798c9100
Ameen Abdulsamad <amenchawshly98@gmail.com>
2022-12-24 03:36:13 +0300

    Feat: add firebase config
    add firebasecnfix to the project
    Fixes #15

---
+++ src/firebase-config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from '@firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDY-EfG2iUbCSILU2iQCAVXYjLmLCI4Pao",
+    authDomain: "calm-minds-project.firebaseapp.com",
+    projectId: "calm-minds-project",
+    storageBucket: "calm-minds-project.appspot.com",
+    messagingSenderId: "717780488615",
+    appId: "1:717780488615:web:36b6d97999c5170e8c2682",
+    measurementId: "G-QXZEMXZ3BL"
+  };
+
+  const app = initializeApp(firebaseConfig);
+  export const db = getFirestore(app)
