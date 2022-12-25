aa9e471a58b07526e447aff5ad4c493ddefc50ed
Ameen Abdulsamad <amenchawshly98@gmail.com>
2022-12-25 20:33:27 +0300

    Build: create about page
    create about page with two components with passed props
    Fixes #17

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import AboutText from './components/AboutText';
+import Founder from './components/Founder';
+import image from './components/images/adilimam.jpg';
 
 function About() {
-  return <h1 className="mx-auto text-center text-7xl">About</h1>;
+
+  const descriptionMain = "At Healing, we believe there is a better way to do things. A more valuable way where customers are earned rather than bought. We're obsessively passionate about it, and our mission is to help people achieve it. We focus on search engine optimization. It's one of the least understood and least transparent aspects of great marketing, and we see that as an opportunity. We're excited to simplify SEO for everyone through our software, education, and community."
+  const descriptionFounder = "Healing was founded by Payam Abubakr in 2021. It was called Healing Online, and started as a blog and an online community where some of the world's therapists shared their research and ideas. We launched the Beginner's Guide to Therapy and our first study, and that hub of industry expertise transformed into a small consulting firm and led us to create the Online Therapist of today!"
+
+  return <div className="">
+    <AboutText title="HEALING!" slogan="some cool one liner !" description={descriptionMain}/>
+    <Founder title='Our Founding' description={descriptionFounder} image={image}/>
+  </div>;
 }
 
 export default About;
---
+++ src/pages/About/components/Founder.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Founder({image, title="Our Founding", description}) {
+  return (
+    <div className='mx-auto text-center sm:px-10 md:px-32 lg:px-48 sm:text-left bg-[#E5E5E5] font-poppins pt-10 flex flex-col place-items-center sm:flex sm:flex-row sm:place-items-start'>
+        <div className='w-2/5  sm:pr-14 pb-10'>
+            <img src={image} alt="Founder Pic" className='rounded-md'/>
+        </div>
+        <div className='w-3/5 pb-10'>
+            <h2 className='text-[#424A4F] font-normal text-5xl pb-7'>{title}</h2>
+            <p className='text-xl'>{description}</p>
+        </div>
+    </div>
+  )
+}
+
+export default Founder
