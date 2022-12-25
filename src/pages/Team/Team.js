de57109ce7d5f7f03258dcad61dae6f08bbd063a
hudahamid <huda.00091581@gmail.com>
2022-12-25 21:33:13 +0300

    test

--- src/pages/Team/Team.js
+++ src/pages/Team/Team.js
@@ -4,25 +4,28 @@ import constTeam from './ConstTeam';
 function Team() {
   return (
     <div >
-      <h1 className="mx-auto text-center text-5xl">
+      <h1 className="mx-auto  text-5xl  py-16 px-10 lg:px-40">  
         WE ARE HEALING, NICE TO MEET YOU!
       </h1>
-      <h4 className="mx-auto text-center text-3xl">Meet the Team!</h4>
-      <div className="flex flex-wrap mt-44">
-       
+      
+      <div className="mt-30 px-4">
+
+      <h4 className=" text-4xl  py-14 px-1  lg:px-40">Meet the Team!</h4>
+       {/* maping over the objects */}
+      <div className="flex flex-wrap px-0 pb-20 lg:ml-40 ">
       {constTeam.map((element) => {
         return (
           <div
             key={element.id}
-            className="flex flex-col border-1 
-       p-3 text-center justify-center items-center mx-1"
+            className="flex flex-col border-solid rounded-lg bg-[#EAF8F9]
+       pb-28  justify-center  mx-2  px-0 sm:mx-2 lg:mx-3"
           >
-            <img className="w-16 h-16" src={element.src} alt={element.alt} />
-            <h4> {element.Name}</h4>
-            <h5>{element.title}</h5>
+            <img className="w-35 h-45" src={element.src} alt={element.alt} />
+            <p className="text-center py-4  text-2xl"> {element.Name}</p>
+            <p   className="text-center " >{element.title}</p>
           </div>
         );
-      })}</div>
+      })}</div></div>
     </div>
   );
 }
