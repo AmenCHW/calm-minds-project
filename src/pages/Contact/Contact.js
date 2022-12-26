d9626779fff146d724f12926811e82320336e118
Danah <osta.danah@gmail.com>
2022-12-26 02:41:59 +0200

    Feat: add header with text + input type radio + create the form

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,37 +1,107 @@
 import React from 'react';
 import contactimage from "./contactimage.png"
 
+const radioObject = [
+  {
+    id: 1,
+    text: "I have a question about the service."
+  },
+  {
+    id: 2,
+    text: "I'm a registered client and I need support."
+  },
+  {
+    id: 3,
+    text: "I'm a counselor interested in joining."
+  },
+  {
+    id: 4,
+    text: "I'm a registered counselor and I need support."
+  },
+  {
+    id: 5,
+    text: "I have a business-related inquiry."
+  },
+  {
+    id: 6,
+    text: "I'm interested in Healing Online for my organization."
+  },
+  {
+    id: 7,
+    text: "I have a billing related question."
+  },
+
+
+
+]
+
 function Contact() {
   return (
     <div className="mx-auto py-14 px-10 lg:px-44">
       <h1 className="pt-5 text-3xl md:text-5xl format-normal leading-normal">
-      SEND US YOUR REQUEST!
+        SEND US YOUR REQUEST!
       </h1>
-      
+
       <p className=" mt-2 text-2xl">
-      Do you have a question, concern, idea, feedback, or problem?  
-      If you need assistance, please fill out the form below and we&apos;d be happy to help!
+        Do you have a question, concern, idea, feedback, or problem?
+        If you need assistance, please fill out the form below and we&apos;d be happy to help!
       </p>
 
-      <div className='flex flex-wrap pt-10 justify-between'>
+      <div className='flex flex-wrap pt-14 justify-between'>
+
         <div>
-           <p className='text-xl font-bold'>Type of contact</p>
-           <div className="flex flex-col">
-              <div className="py-4 text-xl">
-              <input type="checkbox" id="" name="" value="" /> I have a question about the service.
-              </div>
-              
-              <input type="checkbox" shape="round" id="" name="" value="" /> I&apos;m a registered client and I need support.
-              <input type="checkbox" id="" name="" value="" /> I&apos;m a counselor interested in joining.
-              <input type="checkbox" id="" name="" value="" /> I&apos;m a registered counselor and I need support.
-              <input type="checkbox" id="" name="" value="" /> I have a business-related inquiry.
-              <input type="checkbox" id="" name="" value="" /> I&apos;m interested in Healing Online for my organization.
-              <input type="checkbox" id="" name="" value="" /> I have a billing related question.
-           </div>
+          <p className='text-2xl font-semibold mb-4'>Type of contact</p>
+          <div className="flex flex-col">
+            {radioObject.map((item) => {
+              return (
+                <label key={item.id} className="py-3 text-xl" htmlFor="for">
+                  <input type="radio" id="for" name="" value="" />
+                  <span className="ml-2">{item.text}</span>
+                </label>
+              )
+            })}
+          </div>
         </div>
 
-        <img src={contactimage} alt="contactimage" />
+        <img className='mt-3 md:mt-0' src={contactimage} alt="contactimage" />
       </div>
+
+      <div className='flex flex-wrap justify-between mt-12'>
+
+        <form>
+
+          <label className=' flex flex-col' htmlFor="name">
+            <span className="mb-5 text-2xl font-normal">Full Name:</span>
+            <input type="text" id="name" placeholder="Enter your full name here..." className='border-2 rounded-lg h-16 w-[220px] md:w-[604px] border-[#E5E5E5] pl-4
+          placeholder:md:pl-4 placeholder:pl-1' />
+          </label>
+
+        </form>
+
+        <div className='mx-auto my-5 md:my-32'>
+          <div className="flex flex-col bg-[#EAF8F9] border-2 border-[#EAF8F9] rounded-xl p-8 w-52 md:w-96 text-2xl ">
+            <h1 className='font-normal'>
+              Find Us At:
+            </h1>
+            <p className='text-gray-500'>
+              Nergiz Plaza
+            </p>
+            <p className='text-gray-500'>
+              3rd Floor
+            </p>
+            <p className='text-gray-500'>
+              Bakhtiyari Street 40m
+            </p>
+            <p className='text-gray-500'>
+              Erbil, Iraq
+            </p>
+            <p className='text-gray-500'>
+              44001
+            </p>
+          </div>
+        </div>
+      </div>
+
     </div>
   )
 }
