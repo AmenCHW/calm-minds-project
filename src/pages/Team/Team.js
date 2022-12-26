3bcdef1ff82d77ce1d3c70a820e30c053b6174fb
hudahamid <huda.00091581@gmail.com>
2022-12-26 22:15:47 +0300

    style fix

--- src/pages/Team/Team.js
+++ src/pages/Team/Team.js
@@ -8,21 +8,22 @@ function Team() {
         WE ARE HEALING, NICE TO MEET YOU!
       </h1>
       
-      <div className="mt-30 px-4">
+      <div className="mt-30 px-4 ">
 
-      <h4 className=" text-4xl  py-14 px-1  lg:px-40">Meet the Team!</h4>
+      <h4 className=" text-4xl  py-14   lg:px-40 sm:ml-10 sm:justify-center">Meet the Team!</h4>
        {/* maping over the objects */}
-      <div className="flex flex-wrap px-0 pb-20 lg:ml-40 ">
+      <div className="flex flex-wrap px-0 pb-20 lg:ml-40   ">
       {constTeam.map((element) => {
         return (
           <div
             key={element.id}
-            className="flex flex-col border-solid rounded-lg bg-[#EAF8F9]
+            className="flex flex-col border-solid rounded-3xl bg-[#EAF8F9]
        pb-28  justify-center  mx-2  px-0 sm:mx-2 lg:mx-3"
           >
             <img className="w-35 h-45" src={element.src} alt={element.alt} />
+            <hr className='border-[#a3e635] border-4 bg-[#a3e635]'/>
             <p className="text-center py-4  text-2xl"> {element.Name}</p>
-            <p   className="text-center " >{element.title}</p>
+            <p   className="text-center px-20 max-w-xs text-xl" >{element.title}</p>
           </div>
         );
       })}</div></div>
