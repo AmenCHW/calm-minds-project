7623040fcf7f2ae4763c4a8417bd7f3e0dc81f57
Danah <osta.danah@gmail.com>
2022-12-28 03:16:14 +0200

    Feat: create form

--- src/pages/Blogs/Blogs.js
+++ src/pages/Blogs/Blogs.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import EditProfile from '../EditProfile/EditProfile';
 
 function Blogs() {
-  return <h1 className="mx-auto text-center text-7xl">Blogs</h1>;
+  return (
+    <>
+      <EditProfile />
+      <h1 className="mx-auto text-center text-7xl">Blogs</h1>
+    </>
+  )
+  // return <h1 className="mx-auto text-center text-7xl">Blogs</h1>;
 }
 
 export default Blogs;
