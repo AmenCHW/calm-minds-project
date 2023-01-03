3d22e80a8b2f3d3ce180b50043d81638b661dd65
HogrHonar <hogr.it.18@gmail.com>
2023-01-03 21:20:38 +0300

    Welcome Section Page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -5,12 +5,12 @@ import BookingSection from '../Booking/BookingSectionHome';
 import RecentBlogs from '../Blogs/RecentBlogs';
 import Counselor from './Counselor';
 import Ticket from './Ticket';
-
+import Welcome from '../Welcome/welcome';
 
 function Home() {
   return (
     <div>
-      <h1 className="mx-auto text-center text-7xl">Home</h1>
+      <Welcome />
       <BookingSection />
       <Communicate />
       <RecentBlogs />
