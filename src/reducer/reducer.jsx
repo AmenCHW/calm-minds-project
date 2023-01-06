fd7223cec37a48b6d5bdf049deb00d0583fe7b66
HogrHonar <hogr.it.18@gmail.com>
2023-01-06 15:47:42 +0300

    Fixing state redux

--- src/reducer/reducer.jsx
+++ src/reducer/reducer.jsx
@@ -4,18 +4,17 @@ const initalState = {
 };
 
 const reducer = (state = initalState, action = {}) => {
-  
+  switch (action.type) {
+    case 'subscribe':
+      return state(
+        'Thank you for your interest in working with Healing, we have recieved your application.You will receive an email guiding you for the next steps soon after your information is reviewed.'
+      );
+
+    default:
+      break;
+  }
+
   return state;
 };
 
 export default reducer;
-
-// switch (action.type) {
-//     case 'subscribe':
-//       return state(
-//         'Thank you for your interest in working with Healing, we have recieved your application.You will receive an email guiding you for the next steps soon after your information is reviewed.'
-//       );
-
-//     default:
-//       break;
-//   }
