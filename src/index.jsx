22ee302e85e883b2b3925bdd0beabc7241ed96a1
Danah <osta.danah@gmail.com>
2023-01-07 15:05:58 +0200

    Feat: create users and therapists collections in firestore that contains user id

--- src/index.jsx
+++ src/index.jsx
@@ -5,7 +5,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
-import { AuthContextProvider } from './context/AuthContext';
+import { AuthContextProvider } from './context/AuthContext'; 
 
 const container = document.getElementById('root');
 const root = createRoot(container);
