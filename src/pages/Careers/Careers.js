e4f804bdacd86389f40b258fba70554ec4cd7ea9
HogrHonar <hogr.it.18@gmail.com>
2023-01-13 14:54:46 +0300

    Careers page

--- src/pages/Careers/Careers.js
+++ src/pages/Careers/Careers.js
@@ -1,7 +1,99 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '../../firebase-config';
 
 function Careers() {
-  return <h1 className="mx-auto text-center text-7xl">Careers</h1>;
+  const [carees, setCarees] = useState([]);
+  const userCollectionRef = collection(db, 'careers');
+  useEffect(() => {
+    const fetchBlogImage = async () => {
+      const data = await getDocs(userCollectionRef);
+      setCarees(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+
+    fetchBlogImage();
+  }, []);
+
+  return (
+    <div className="">
+      <div className="mx-auto lg:max-w-7xl px-10 py-10">
+        <div className="mb-24">
+          <h1 className="font-medium text-6xl">CAREERS AT HEALING</h1>
+          <p className="text-3xl opacity-50">
+            Be a part of making people feel better.
+          </p>
+        </div>
+
+        <p className="text-[1.2vw]">
+          Our SEO software cuts through mountains of data to surface critical
+          insights. We build and maintain systems that process massive amounts
+          of data (we&#39;re talking 36 trillion records per day and multiple
+          petabytes of storage.) We model transparent and empathetic marketing
+          for the world. We educate our community, making every effort to help
+          them improve their skill. And we do it all by fostering a culture that
+          encourages accountability, empathy, and transparency.
+        </p>
+        <div>
+          <p className="mt-16 text-[1vw]">What role will you play?</p>
+
+          <Link to="/bookingPage" className="focus:text-[#FEE89E]">
+            <button
+              className="lg:mt-10 md:mt-5 bg-[#2DD3E3] hover:bg-blue-400 text-white 
+                  font-bold py-2 text-[1vw] px-4 border-b-4 border-[#2DD3E3] hover:border-blue-500 rounded"
+              type="button"
+            >
+              SEE OUR OPEN LIST
+            </button>
+          </Link>
+        </div>
+      </div>
+
+      <div className="w-full place-items-center py-10 flex flex-col bg-[#FEE89E] mb-10  sm:flex-row sm:p-10 sm:pl-20 sm:pr-8">
+        <div className="w-full mx-auto lg:max-w-7xl px-10  ">
+          <h1 className="text-4xl text-[#424A4F]">OUR HIRING PHILOSOPHY</h1>
+          <p className="text-2xl text-[#424A4F] mt-8">
+            To build the very best SEO tools, we need a workforce that reflects
+            the diversity of our communities and customers. We want Online
+            Therapy to be a place where everyone feels welcome and engaged, bar
+            none. It&#39;s our mission and promise to interview a diverse and
+            representative slate of candidates before making an offer for our
+            open roles.
+          </p>
+        </div>
+      </div>
+      <div className="mx-auto lg:max-w-7xl px-10 py-10">
+        <h1 className="text-4xl text-[#424A4F]">CURRENT OPEN POSITIONS</h1>
+        <p className="text-2xl opacity-50">
+          Please send us an email with the application title as the subject with
+          an attached CV in PDF format!
+        </p>
+      </div>
+      <div>
+        {carees.map((caree) => {
+          return (
+            <div className="mx-auto lg:max-w-7xl px-10 py-4">
+              <div className="flex justify-between ">
+                <div className="flex text-center space-x-4 text-[#2DD3E3] text-3xl">
+                  <h1>{caree.work} |</h1>
+                  <h1>{caree.workon} |</h1>
+                  <h1>{caree.place}</h1>
+                </div>
+                <div>
+                  <h1 className="text-[#2DD3E3] text-3xl">{caree.degree}</h1>
+                </div>
+              </div>
+              <div className="flex justify-between mb-6">
+                <p className="text-2xl">{caree.descriptions}</p>
+                <h1 className="text-2xl ">{caree.workon}</h1>
+              </div>
+              <hr />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default Careers;
