83c1c3b0779395ee79fc8f268e57f847095826b9
Danah <osta.danah@gmail.com>
2023-01-16 03:35:05 +0200

    Feat: Step 4 Specific Qualities & Step 5 Issues

---
+++ src/pages/Booking/BookingForms.js
@@ -0,0 +1,154 @@
+import React, { useState } from 'react';
+import { counselingTypeObject1, relationshipStatusObject2, therapyBeforeObject3, SpecificQualitiesObject4, issuesObject5 } from './BookingFormsObject';
+
+function BookingForms({ step, setStep, formData, setFormData, handleSubmit }) {
+    const [checked, setChecked] = useState('')
+
+    const ChangeFormQuestion = () => {
+        let formQuestion;
+
+        if (step === 1) {
+            formQuestion = "What Type Of Counseling Are You Looking For?";
+        } else if (step === 2) {
+            formQuestion = "What Is Your Relationship Status?";
+        } else if (step === 3) {
+            formQuestion = "Have You Ever Been In Therapy Before?";
+        } else if (step === 4) {
+            formQuestion = "Are There Any Specific Qualities That You'd Like In A Counselor?";
+        } else if (step === 5) {
+            formQuestion = "Are There Any Issues You'd Like To Focus On?";
+        }
+        // eslint-disable-next-line
+        //console.log(formQuestion)
+        return formQuestion;
+    }
+
+    // eslint-disable-next-line
+    console.log("step", step)
+
+    // eslint-disable-next-line
+    console.log("formData", formData)
+
+    function DisplayRadioButtons() {
+        let radioButtons;
+        if (step === 1) {
+            radioButtons = counselingTypeObject1.map(({ id, value, label }) => {
+                return (
+                    <div className="form-check pt-4 text-2xl sm:text-3xl font-normal" key={id}>
+                        <input
+                            type="radio"
+                            name="exampleRadios"
+                            id={id}
+                            value={value}
+                            checked={value === checked}
+                            onChange={() => { setChecked(value); setFormData({ ...formData, counseling_type: value }) }}
+                        />
+                        <label className="form-check-label" htmlFor={id}>
+                            {label}
+                        </label>
+                    </div>
+                )
+            })
+        } else if (step === 2) {
+            radioButtons = relationshipStatusObject2.map(({ id, value, label }) => {
+                return (
+                    <div className="form-check pt-4 text-2xl sm:text-3xl font-normal" key={id}>
+                        <input
+                            type="radio"
+                            name="exampleRadios"
+                            id={id}
+                            value={value}
+                            checked={value === checked}
+                            onChange={() => { setChecked(value); setFormData({ ...formData, relationship_status: value }) }}
+                        />
+                        <label className="form-check-label" htmlFor={id}>
+                            {label}
+                        </label>
+                    </div>
+                )
+            })
+        } else if (step === 3) {
+            radioButtons = therapyBeforeObject3.map(({ id, value, label }) => {
+                return (
+                    <div className="form-check pt-4 text-2xl sm:text-3xl font-normal" key={id}>
+                        <input
+                            type="radio"
+                            name="exampleRadios"
+                            id={id}
+                            value={value}
+                            checked={value === checked}
+                            onChange={() => { setChecked(value); setFormData({ ...formData, ever_been_in_therapy_before: value }) }}
+                        />
+                        <label className="form-check-label" htmlFor={id}>
+                            {label}
+                        </label>
+                    </div>
+                )
+            })
+        } else if (step === 4) {
+            radioButtons = SpecificQualitiesObject4.map(({ id, value, text }) => {
+                return (
+                    <div key={id} className="flex py-3">
+                        <input
+                            key={id}
+                            type="radio"
+                            htmlFor="for"
+                            id={id}
+                            name="type"
+                            value={value}
+                            checked={value === checked}
+                            onChange={() => { setChecked(value); setFormData({ ...formData, counselor_qualities: value }) }}
+                        />
+                        <p className="ml-3 text-xl">{text}</p>
+                    </div>
+                );
+            })
+        } else if (step === 5) {
+            radioButtons = issuesObject5.map(({ id, value, text }) => {
+                return (
+                    <div key={id} className="flex py-3">
+                        <input
+                            key={id}
+                            type="radio"
+                            htmlFor="for"
+                            id={id}
+                            name="type"
+                            value={value}
+                            checked={value === checked}
+                            onChange={() => { setChecked(value); setFormData({ ...formData, issues: value }) }}
+                        />
+                        <p className="ml-3 text-xl">{text}</p>
+                    </div>
+                );
+            })
+        }
+        return radioButtons;
+    }
+
+
+    return (
+
+        <div>
+            <form onSubmit={handleSubmit} className='p-6 mt-9 mb-12 mx-auto w-auto md:w-[700px] h-[500px] border-2 border-[#E5E5E5] rounded-md shadow-lg bg-white'>
+                <h1 className='text-2xl sm:text-3xl font-normal pb-8 text-left'>
+                    {ChangeFormQuestion()}
+                </h1>
+
+                <div className='flex flex-col'>
+                    {DisplayRadioButtons()}
+
+                    <button type="submit"
+                        disabled={step > 7}
+                        onClick={() => { setStep((currentStep) => currentStep + 1) }}
+                        className='bg-[#2DD3E3] text-1xl sm:text-2xl px-6 py-3 font-normal border-2 border-[#2DD3E3] rounded-md place-self-start mt-5'>
+                        NEXT
+                    </button>
+                </div>
+            </form>
+        </div >
+
+    )
+}
+
+export default BookingForms;
+
--- src/pages/Booking/BookingPage.js
+++ src/pages/Booking/BookingPage.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import FirstSecondThirdForm from './FirstSecondThirdForm';
+import React, { useState, useEffect } from 'react';
+import BookingForms from './BookingForms';
 import "./BookingRadioButtonStyle.css"
 
 function Booking() {
@@ -10,86 +10,54 @@ function Booking() {
   // }
 
   const [step, setStep] = useState(1)
+  const [pageTitle, setPageTitle] = useState('')
+  const [pageText, setPageText] = useState('')
 
   const [formData, setFormData] = useState({
-    counseling_type: "",
-    relationship_status: "",
-    ever_been_in_therapy_before: "",
-    counselor_qualities: "",
-    issues: "",
-    what_brings_you_here: "",
+    counseling_type: null,
+    relationship_status: null,
+    ever_been_in_therapy_before: null,
+    counselor_qualities: null,
+    issues: null,
+    what_brings_you_here: null,
   })
 
-  const ChangePageTitle = () => {
-    let pageTitle;
-
-    if (step === 1 || step === 2 || step === 3 || step === 4 || step === 5) {
-      pageTitle = "LET'S MATCH YOU WITH THE RIGHT THERAPIST";
-    } else if (step === 6) {
-      pageTitle = "WHAT BRINGS YOU HERE?";
-    } else if (step === 7) {
-      pageTitle = "SUBMIT YOUR APPOINTMENT";
-    } else if (step === 8) {
-      pageTitle = "YOUR REQUEST HAS BEEN SUBMITTED";
-    }
-    // eslint-disable-next-line
-    // console.log(pageTitle)
-    return pageTitle;
+  const handleSubmit = (e) => {
+    e.preventDefault();
   }
 
-  const ChangePageText = () => {
-    let pageText;
 
+  useEffect(() => {
     if (step === 1 || step === 2 || step === 3 || step === 4 || step === 5) {
-      pageText = "Please fill out this short questionnaire to provide some general and anonymous background about you and the issues you'd like to deal with in online therapy. It would help us match you with the most suitable therapist for you.";
+      setPageTitle("LET'S MATCH YOU WITH THE RIGHT THERAPIST");
+      setPageText("Please fill out this short questionnaire to provide some general and anonymous background about you and the issues you'd like to deal with in online therapy. It would help us match you with the most suitable therapist for you.");
     } else if (step === 6) {
-      pageText = "Please specify (in a few sentences) why you'd like counseling.This will give your counselor a good understanding of where to start.";
+      setPageTitle("WHAT BRINGS YOU HERE?");
+      setPageText("Please specify (in a few sentences) why you'd like counseling.This will give your counselor a good understanding of where to start.")
     } else if (step === 7) {
-      pageText = "Click Submit if you are sure of all your choices.";
+      setPageTitle("SUBMIT YOUR APPOINTMENT");
+      setPageText("Click Submit if you are sure of all your choices.");
     } else if (step === 8) {
-      pageText = "You will receive an email guiding you to book a date and time soon.";
+      setPageTitle("YOUR REQUEST HAS BEEN SUBMITTED");
+      setPageText("You will receive an email guiding you to book a date and time soon.");
     }
-    // eslint-disable-next-line
-    // console.log(pageText)
-    return pageText;
-  }
-
-  // const ChangeFormQuestion = () => {
-  //   let formQuestion;
-
-  //   if (step === 1) {
-  //     formQuestion = "What Type Of Counseling Are You Looking For?";
-  //   } else if (step === 2) {
-  //     formQuestion = "What Is Your Relationship Status?";
-  //   } else if (step === 3) {
-  //     formQuestion = "Have You Ever Been In Therapy Before?";
-  //   } else if (step === 4) {
-  //     formQuestion = "Are There Any Specific Qualities That You'd Like In A Counselor?";
-  //   } else if (step === 5) {
-  //     formQuestion = "Are There Any Issues You'd Like To Focus On?";
-  //   }
-  //   // eslint-disable-next-line
-  //   // console.log(formQuestion)
-  //   return formQuestion;
-  // }
+  }, [step]);
 
 
-  // eslint-disable-next-line
-  console.log("Form Data", formData)
 
   return (
     <div className='mx-auto lg:max-w-7xl px-10 py-10'>
 
       <h1 className='pt-5 text-3xl md:text-5xl format-normal leading-normal'>
-        {ChangePageTitle()}
+        {pageTitle}
       </h1>
 
       <p className='mt-2 text-2xl text-gray-500'>
-        {ChangePageText()}
+        {pageText}
       </p>
 
       <div>
-        <FirstSecondThirdForm step={step} setStep={setStep} formData={formData} setFormData={setFormData} />
+        <BookingForms step={step} setStep={setStep} formData={formData} setFormData={setFormData} handleSubmit={handleSubmit} />
       </div>
 
     </div>
