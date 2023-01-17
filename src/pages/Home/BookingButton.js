914de0e58932f803bceec6caad6aed0b2d7f894f
Danah <osta.danah@gmail.com>
2023-01-17 05:24:04 +0200

    Feat: apply conditional rendering on button
    if there is a user then show book an appointment else show sign up on the button of 2nd section of home page

--- src/pages/Home/BookingButton.js
+++ src/pages/Home/BookingButton.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { UserAuth } from '../../context/AuthContext';
+
 
 function BookingButton() {
+
+  const { user } = UserAuth();
+
   return (
     <div className="Booking">
-      <Link to="/bookingPage" className="focus:text-[#FEE89E]">
-        <button
-          className="mt-20 bg-[#2DD3E3] hover:bg-blue-400 text-white 
+
+      <button
+        className="mt-20 bg-[#2DD3E3] hover:bg-blue-400 text-white 
                   font-bold py-2 px-4 border-b-4 border-[#2DD3E3] hover:border-blue-500 rounded"
-          type="button"
-        >
-          BOOK AN APPOINTMENT
-        </button>
-      </Link>
+        type="button"
+      >
+        {user ? <Link to="/bookingPage" className="focus:text-[#FEE89E]">BOOK AN APPOINTMENT</Link> : <Link to="/signup" className="focus:text-[#FEE89E]">SIGN UP</Link>}
+      </button>
+
     </div>
   );
 }
