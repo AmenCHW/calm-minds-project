2bdffe833f973a9b2294f42fb772149acef2925c
Ameen Abdulsamad <amenchawshly98@gmail.com>
2023-01-17 12:21:09 +0300

    Feat: add upload ID to firebase storage

--- src/pages/EditProfile/EditProfile.js
+++ src/pages/EditProfile/EditProfile.js
@@ -1,7 +1,54 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { storage } from '../../firebase-config';
 import profileimage from './profileimage.png';
 
 function EditProfile() {
+
+  const [file, setFile] = useState('')
+  useEffect(()=>{
+    const uploadFile = ()=> {
+
+      const name = new Date().getTime() + file.name
+      console.log(name);
+      const storageRef = ref(storage, file.name);
+      const uploadTask = uploadBytesResumable(storageRef, file.name);
+
+      uploadTask.on('state_changed', 
+  (snapshot) => {
+    // Observe state change events such as progress, pause, and resume
+    // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+    console.log( `Upload is ${progress} % done`);
+    switch (snapshot.state) {
+      case 'paused':
+        console.log('Upload is paused');
+        break;
+      case 'running':
+        console.log('Upload is running');
+        break;
+        default:
+          break;
+    }
+  }, 
+  (error) => {
+    console.log(error)
+  }, 
+  () => {
+    // Handle successful uploads on complete
+    // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+      console.log('File available at', downloadURL);
+    });
+  }
+);
+
+
+    };
+    // eslint-disable-next-line
+    file && uploadFile();
+  }, [file])
+
   return (
     <div className="mx-auto lg:max-w-7xl px-10 py-10">
       <h1 className="text-center text-[#FF0000] text-2xl mb-9">
@@ -143,6 +190,7 @@ function EditProfile() {
                 <input
                   type="file"
                   id="image"
+                  onChange={(e)=> setFile(e.target.files[0])}
                   className="border-2 rounded-lg h-16 w-1/2 lg:w-[470px] items-center py-auto border-gray-100 pl-4 shadow-md"
                 />
               </label>
