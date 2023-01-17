7a523e65d5f0386bd2d2064f542aa6504c9491f2
hudahamid <huda.00091581@gmail.com>
2023-01-17 22:46:27 +0300

    try now

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { collection, query, where, getDocs } from "firebase/firestore";
 import { db } from '../../firebase-config';
