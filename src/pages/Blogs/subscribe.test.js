71c6ea5db03a996d958647f6d2a3ab13c08fec13
hudahamid <huda.00091581@gmail.com>
2023-01-22 17:19:57 +0300

    react-hook testing testing email intial input

---
+++ src/pages/Blogs/subscribe.test.js
@@ -0,0 +1,46 @@
+import React from "react";
+ import {fireEvent} from "@testing-library/react"
+import  SubcribedEmails from "./SubcribedEmails";
+import renderer from 'react-test-renderer' 
+import { renderHook } from "@testing-library/react-hooks";
+
+
+test("first snapshot testing",()=>{
+    const component=renderer.create( < SubcribedEmails/>).toJSON()  
+    expect( component).toMatchSnapshot();
+})
+
+const btn=()=>{fireEvent(
+   
+    getByRole(container,  "button",),
+    new MouseEvent('click', {
+      bubbles: true,
+      cancelable: true,
+    }),
+    
+  )
+}
+console.log(btn)
+
+
+// 1st time
+  describe("when rendered", () => {
+    it("expect email to be empty", () => {
+      const { result } = renderHook(
+        () => SubcribedEmails("Test string"));
+      
+      expect(result.current.newEmailInput).toEqual(undefined);
+    });
+  });
+
+
+
+ 
+// // 2nd time
+// test('expect email to be empty', () => {
+//     const { result } = renderHook(() => SubcribedEmails())
+  
+//     expect(result.current.newEmailInput).toBe("")
+    
+//   })
+ 
