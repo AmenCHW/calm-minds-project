0b3b83ce2e8f4f88837d178d461f72f3f1d6818a
hudahamid <huda.00091581@gmail.com>
2023-01-23 21:31:14 +0300

    UnitTest testing titles

--- src/pages/About/About.test.js
+++ src/pages/About/About.test.js
@@ -1,7 +1,25 @@
 import React from "react";
-// import {render} from "@testing-library/react"
+ import {render, screen} from "@testing-library/react"
 import About from "./About";
 import renderer from 'react-test-renderer' 
+
+
+test("test healing", async() => {
+    render(
+       <About title ="about" />
+   );
+ const headingElement = screen.getAllByText(/about/i);
+   expect(headingElement).toBeInTheDocument;
+ });
+
+ test("test healing", async() => {
+    render(
+       <About title ="heaing" />
+   );
+ const headingElement = screen.getAllByText(/healing/i);
+   expect(headingElement).toBeInTheDocument;
+ });
+
 
 test("first snapshot testing",()=>{
     const component=renderer.create( <About/>).toJSON()  
@@ -9,13 +27,7 @@ test("first snapshot testing",()=>{
 })
 
 
-// test("test healing", async() => {
-//     render(
-//        <About title ="Healing" />
-//    );
-//  const headingElement = screen.getAllByText(/healing/i);
-//    expect(headingElement).toBeInTheDocument;
-//  });
+
  
  
 //  it("test <p> ", async() => {
--- src/pages/Requirement/Requirenment.test.js
+++ src/pages/Requirement/Requirenment.test.js
@@ -3,10 +3,14 @@ import React from "react";
 import Requirement from "./RequirementPage";
 import renderer from 'react-test-renderer' 
 
+
+
 test("first snapshot testing",()=>{
     const component=renderer.create( <Requirement/>).toJSON()  
     expect( component).toMatchSnapshot();
 })
+
+
 
 // test("render 5 requirenments",()=>{
 //     const list=screen.getAllByRole("listitem")
