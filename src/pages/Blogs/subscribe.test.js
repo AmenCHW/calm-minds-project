5d1d83fd8dbff72d35656eedcb529b1e0e70643f
hudahamid <huda.00091581@gmail.com>
2023-01-24 21:52:06 +0300

    renderHook test comment

--- src/pages/Blogs/subscribe.test.js
+++ src/pages/Blogs/subscribe.test.js
@@ -1,7 +1,7 @@
 import {render,screen} from "@testing-library/react"
 import SubcribedEmails from "./SubcribedEmails";
 import userEvent from '@testing-library/user-event';
-
+// import { renderHook } from "@testing-library/react-hooks";
 
 
 // __________________ Testing Email's button ____________________________-
@@ -12,3 +12,28 @@ it("should show error message when all the fields are not entered", () => {
     userEvent.click(buttonElement);
   });
  
+
+
+//    const btn=()=>{fireEvent(
+
+//     getByRole(container,  "button",),
+//     new MouseEvent('click', {
+//       bubbles: true,
+//       cancelable: true,
+//     }),
+
+//   )
+// }
+// console.log(btn)
+
+
+// // 1st time
+//   describe("when rendered", () => {
+//     it("expect email to be empty", () => {
+//       const { result } = renderHook(
+//         () => SubcribedEmails("Test string"));
+
+//       expect(result.current.newEmailInput).toEqual(undefined);
+//     });
+//   });
+
