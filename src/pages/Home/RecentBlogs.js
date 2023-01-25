edcb97c766d308846564209c0be2197635981a7d
Danah <osta.danah@gmail.com>
2023-01-25 14:26:06 +0200

    Remove carousal

--- src/pages/Home/RecentBlogs.js
+++ src/pages/Home/RecentBlogs.js
@@ -17,63 +17,7 @@ function RecentBlogs() {
   }, []);
   //  _________________________________________ Carsoul_____________________________________________________________________________________________________________
 
-  const handleRightClick = () => {
-    const prevState = [...blogs];
-    // find next inactive card index - top
-    const nextCardIdx = prevState
-      .filter((ft) => ft.active === true)
-      .sort((a, b) => {
-        if (a.pos > b.pos) {
-          return 1;
-        }
-        return -1;
-      })[0].idx;
-    // reset
-    prevState.find((f) => f.active === false).active = true;
-    // update
-    prevState.find((f) => f.idx === nextCardIdx).active = false;
-    // maximize pos
-    prevState.find((f) => f.idx === nextCardIdx).pos =
-      Math.max.apply(
-        null,
-        prevState.map((o) => {
-          return o.pos;
-        })
-      ) + 1;
 
-    // update state
-    setBlog(prevState);
-  };
-
-  const handleLeftClick = () => {
-    const prevState = [...blogs];
-    // find next inactive card index - bottom
-    const nextCardIdx = prevState
-      .filter((ft) => ft.active === true)
-      .sort((a, b) => {
-        if (a.pos > b.pos) {
-          return 1;
-        }
-        return -1;
-      })
-      .pop(1).idx;
-
-    // minimize pos
-    prevState.find((f) => f.active === false).pos =
-      Math.min.apply(
-        null,
-        prevState.map((o) => {
-          return o.pos;
-        })
-      ) - 1;
-    // reset
-    prevState.find((f) => f.active === false).active = true;
-    // update
-    prevState.find((f) => f.idx === nextCardIdx).active = false;
-
-    // update state
-    setBlog(prevState);
-  };
   return (
     <div className=" bg-[#EAF8F9]">
       <div className="mx-auto lg:max-w-7xl px-10 py-10">
@@ -81,13 +25,7 @@ function RecentBlogs() {
           RECENT BLOGS
         </h1>
         <div className="flex flex-col md:flex-row place-content-center mb-4">
-          <button
-            type="button"
-            className="text-xl md:text-5xl cursor-pointer border-2 pb-2 px-5 border-black rounded-md h-1/2 my-10 md:mr-4 mx-36 md:mx-0"
-            onClick={() => handleLeftClick()}
-          >
-            &#8249;
-          </button>
+
 
           <div className="flex-col flex md:flex-row gap-2 sm:gap-4 place-items-center">
             {blogs.map((blog) => {
@@ -108,13 +46,6 @@ function RecentBlogs() {
             })}
           </div>
 
-          <button
-            type="button"
-            className="text-xl md:text-5xl hover:cursor-pointer border-2 pb-2 px-5 border-black rounded-md h-1/2 my-10 md:ml-4 mx-36 md:mx-0"
-            onClick={() => handleRightClick()}
-          >
-            &#8250;
-          </button>
         </div>
       </div>
     </div>
