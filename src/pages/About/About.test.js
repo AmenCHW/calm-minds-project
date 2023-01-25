79e10219047350d0522e5ab6e1f10c97293d5e4a
hudahamid <huda.00091581@gmail.com>
2023-01-25 12:42:58 +0300

    blog ,about

--- src/pages/About/About.test.js
+++ src/pages/About/About.test.js
@@ -1,27 +1,37 @@
+
 import React from "react";
-// import {render} from "@testing-library/react"
+import {render,screen} from "@testing-library/react"
 import About from "./About";
 import renderer from 'react-test-renderer' 
 
-test("first snapshot testing",()=>{
+// _________________getAllByText_________________
+test("test healing", async() => {
+    render(
+       <About title ="Healing" />
+   );
+ const headingElement = screen.getAllByText(/healing/i);
+   expect(headingElement).toBeInTheDocument;
+ });
+
+
+// _________________testing title with getByText_________________
+it("test <p> ", async() => {
+     render(
+        <About title="about" />
+  );
+    const headingElement = screen.getByText(/About/i);  //case sensittive?
+    expect(headingElement).toBeInTheDocument();
+   });
+
+
+   // last one ,snapshot
+ test("first snapshot testing",()=>{
     const component=renderer.create( <About/>).toJSON()  
     expect( component).toMatchSnapshot();
 })
 
 
-// test("test healing", async() => {
-//     render(
-//        <About title ="Healing" />
-//    );
-//  const headingElement = screen.getAllByText(/healing/i);
-//    expect(headingElement).toBeInTheDocument;
-//  });
- 
- 
-//  it("test <p> ", async() => {
-//      render(
-//         <About paragraph ="some cool one liner!" />
-//     );
-//     const headingElement = screen.getByText(/About/i);  //case sensittive?
-//     expect(headingElement).toBeInTheDocument();
-//    });
+test('first snapshot testing', () => {
+  const component = renderer.create(<About />).toJSON();
+  expect(component).toMatchSnapshot();
+});
